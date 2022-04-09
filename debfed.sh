79d8e82afcd81d0780c2e03377cb216ff3d18fd1
winzic <sealprime1@gmail.com>
2022-04-09 11:09:25 +0200

    Update debfed.sh

--- debfed.sh
+++ debfed.sh
@@ -27,19 +27,3 @@ debupgradelist()
 {
     
 }
-
-# This are functions for fedora operating system
-fedpkgs()
-{
-
-}
-
-auto_fedupdate()
-{
-    dnf update -yy
-}
-
-auto_fedupgrade()
-{
-    dnf upgrade -yy
-}
