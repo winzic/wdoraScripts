9db5c0c5328a7fdc52e7badbf43d1f7c67044d54
winzic <sealprime1@gmail.com>
2022-05-14 11:52:58 +0200

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -27,15 +27,18 @@ sncte_packages()
 # Generic Programming and Development Tools/Libraries
 gpdtl_packages()
 {
-    dnf install gcc flex bison \
-                openmp pvm torque 
+    dnf install gcc flex.x86_64 bison.x86_64 bison-devel.x86_64 \
+                openmpi-devel.x86_64 parallel.noarch torque.x86_64 \
+                openmpi.x86_64 torque-client.x86_64 torque-devel.x86_64 \
+                torque-docs.noarch torque-gui.x86_64 torque-libs.x86_64 \
+                torque-scheduler.x86_64 torque-server.x86_64
                 
 }
 
 # Editing, Plotting, Drawing and Visualization
 epdv_packages()
 {
-    dnf install texmaker kile lyx \
+    dnf install texmaker.x86_64 kile.x86_64 lyx.x86_64 \
                 scribus inkscape scite \
                 xfig gnuplot dia \
                 ggobi g3data gimp \
