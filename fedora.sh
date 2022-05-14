e767d8218cf4679ac3e682bd2bf64297f66fd6aa
winzic <sealprime1@gmail.com>
2022-05-14 15:10:26 +0200

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -31,7 +31,10 @@ gpdtl_packages()
                 openmpi-devel.x86_64 parallel.noarch torque.x86_64 \
                 openmpi.x86_64 torque-client.x86_64 torque-devel.x86_64 \
                 torque-docs.noarch torque-gui.x86_64 torque-libs.x86_64 \
-                torque-scheduler.x86_64 torque-server.x86_64
+                torque-scheduler.x86_64 torque-server.x86_64 colorgcc.noarch \
+                cross-gcc-common.noarch gcc-x86_64-linux-gnu.x86_64 gcc-gfortran.x86_64 \
+                libgomp.x86_64 libquadmath.x86_64 libquadmath-devel.x86_64 gcc.x86_64 \
+                libquadmath-devel.x86_64
                 
 }
 
@@ -39,8 +42,9 @@ gpdtl_packages()
 epdv_packages()
 {
     dnf install texmaker.x86_64 kile.x86_64 lyx.x86_64 \
-                scribus inkscape scite \
-                xfig gnuplot dia \
+                scribus.x86_64 scribus-generator.noarch inkscape.x86_64 scite \
+                inkscape-docs.x86_64 inkscape-psd.noarch inkscape-view.x86_64 \
+                xfig.x86_64 gnuplot.x86_64 gnuplot-doc.noarch plotutils.x86_64 dia.x86_64 \
                 ggobi g3data gimp \
                 mayavi
 }
